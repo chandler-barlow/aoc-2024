a4d248859e7ed64f3a2e68dc1ee7cf7bf04efbfc
Chandler Barlow <chandlerbrlw@gmail.com>
2024-12-05 00:49:19 -0800

    fml

--- app/Day/Four.hs
+++ app/Day/Four.hs
@@ -5,6 +5,10 @@ type Board a = [((Int, Int), a)]
 
 data Step = Step (Int, Int) [Step] deriving Show
 
+type Cols = Int
+type Rows = Int 
+type Depth = Int
+
 index :: Step -> (Int, Int)
 index (Step (x,y) _) = (x,y)
 
@@ -13,47 +17,51 @@ children (Step _ xs) = xs
 
 -- | This generates all possibles paths that can be taken on a game board
 -- Includes loops and cycles
-allSteps :: Int -> Int -> Step
-allSteps n m = go n m 0 0
+steps :: Rows -> Cols -> Coord -> Step
+steps n m coord = go n m coord 
   where steps = [subtract 1, (+ 1), id]
         inBounds (x,y)
           | x < 0 || y < 0 = False
           | x > n || y > m = False
           | otherwise = True
-        go n m i j = 
+        go n m (i, j) = 
           let validSteps = filter inBounds next
               next = do 
                 i' <- steps <*> pure i
                 j' <- steps <*> pure j
                 pure (i', j')
-              xs = map (uncurry $ go n m) validSteps          
+              xs = map (go n m) validSteps          
           in Step (i, j) xs
 
-type Cols = Int
-type Rows = Int 
-type Depth = Int
+allSteps :: Rows -> Cols -> [Step]
+allSteps r c = do
+    i <- [0..r]
+    j <- [0..c]
+    pure $ steps r c (i, j)
 
--- | Never walks the same ground twice
-finite :: Step -> Step
-finite (Step x xs) = Step x $ go [index x] xs
-  where go seen xs = 
-          let valid = filter (\x -> index x `notElem` seen) xs
-              
-
--- All non backtracking walks of depth n
-walkBoard :: Rows -> Cols -> Depth -> Step -> [[Step]] 
-walkBoard n m depth start = go [] depth start 
- where  go :: [(Int, Int)] -> Depth -> Step -> [[Step]]
+-- | Limit a step tree to just non repeating walks at depth n
+limitSteps :: Depth -> Step -> [Step] 
+limitSteps depth start = go [] depth start 
+ where  go :: [(Int, Int)] -> Depth -> Step -> [Step]
         go seen n (Step x xs)
-          | (index x) `elem` seen 
-          |  n == 1 = []
+          | x `elem` seen = []
+          | n == 1 = [Step x []]
           | otherwise = 
-              let x' = index x
-                  next = go (n - 1) (x':seen) xs
-              in map (x:) next
+              let next = go (x:seen) (n - 1) <$> xs
+              in Step x <$> next
 
--- allCombos :: Int -> Board a -> [[a]]
--- allCombos depth = undefined
+type Coord = (Int, Int)
+
+type Board a = M.Map Coord a
+
+-- | Apply a path to a board
+takeWalk :: Board a -> Step -> [[a]]
+takeWalk board (Step _ []) = []
+takeWalk board (Step x xs) = (board ! x) : (takeWalk board <$> xs)
+
+-- | Look through a grid for a path that finds the sequence
+wordSearch :: Eq a => Board a -> [a] -> [[Coord]]
+wordSearch board word = undefined
 
 -- parse :: String -> [[Char]]
 -- parse input = 
--- test.hs
+++ test.hs
@@ -7,6 +7,7 @@ index (Step (x,y) _) = (x,y)
 children :: Step -> [Step]
 children (Step _ xs) = xs
 
+
 -- | This generates all possibles paths that can be taken on a game board
 allSteps :: Int -> Int -> Step
 allSteps n m = go n m 0 0
@@ -24,3 +25,17 @@ allSteps n m = go n m 0 0
               xs = map (uncurry $ go n m) validSteps          
           in Step (i, j) xs
 
+type Cols = Int
+type Rows = Int 
+type Depth = Int
+
+-- | Limit a step tree to just non repeating walks at depth n
+limitSteps :: Depth -> Step -> [Step] 
+limitSteps depth start = go [] depth start 
+ where  go :: [(Int, Int)] -> Depth -> Step -> [Step]
+        go seen n s@(Step x xs)
+          | x `elem` seen = []
+          | n == 1 = [Step x []]
+          | otherwise = 
+              let next = go (x:seen) (n - 1) <$> xs
+              in Step x <$> next
